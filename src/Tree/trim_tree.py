7c6ed23a327a5aef49cd1edf96c1d751bd351a1b
Jianbo Sun <js7995@nyu.edu>
2019-09-04 21:47:00 -0400

    add parts of questions

---
+++ src/Tree/trim_tree.py
@@ -0,0 +1,119 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Binary_Tree_Pruning_814:
+    def pruneTree(self, root: TreeNode) -> TreeNode:
+        self.trim(root)
+        return root
+
+    def trim(self,root:TreeNode) -> bool:
+        if root is None:
+            return False
+        left_has_one = self.trim(root.left)
+        right_has_one = self.trim(root.right)
+
+        if not left_has_one:
+            root.left = None
+
+        if not right_has_one:
+            root.right = None
+
+        return root.val ==1 or left_has_one or right_has_one
+
+
+
+class Solution:
+    def trimBST(self, root: TreeNode, L: int, R: int) -> TreeNode:
+        if not root:
+            return None
+
+        if root.val > R:
+            return self.trimBST(root.left, L, R)
+        elif root.val < L:
+            return self.trimBST(root.right, L, R)
+        else:
+            root.left = self.trimBST(root.left, L, R)
+            root.right = self.trimBST(root.right, L, R)
+            return root
+
+def stringToTreeNode(input):
+    input = input.strip()
+    input = input[1:-1]
+    if not input:
+        return None
+
+    inputValues = [s.strip() for s in input.split(',')]
+    root = TreeNode(int(inputValues[0]))
+    nodeQueue = [root]
+    front = 0
+    index = 1
+    while index < len(inputValues):
+        node = nodeQueue[front]
+        front = front + 1
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            leftNumber = int(item)
+            node.left = TreeNode(leftNumber)
+            nodeQueue.append(node.left)
+
+        if index >= len(inputValues):
+            break
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            rightNumber = int(item)
+            node.right = TreeNode(rightNumber)
+            nodeQueue.append(node.right)
+    return root
+
+
+def treeNodeToString(root):
+    if not root:
+        return "[]"
+    output = ""
+    queue = [root]
+    current = 0
+    while current != len(queue):
+        node = queue[current]
+        current = current + 1
+
+        if not node:
+            output += "null, "
+            continue
+
+        output += str(node.val) + ", "
+        queue.append(node.left)
+        queue.append(node.right)
+    return "[" + output[:-2] + "]"
+
+
+def main():
+    import sys
+    import io
+    def readlines():
+        for line in io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8'):
+            yield line.strip('\n')
+
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            root = stringToTreeNode(line)
+
+            ret = Solution().pruneTree(root)
+
+            out = treeNodeToString(ret)
+            print(out)
+        except StopIteration:
+            break
+
+
+if __name__ == '__main__':
+    main()
